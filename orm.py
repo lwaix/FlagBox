28b3f269be0fa66443a26e9a74f0d9defaa73bc9
lwaix <1494645263@qq.com>
2019-01-19 22:11:20 +0800

    完善

--- orm.py
+++ orm.py
@@ -9,15 +9,21 @@ and or?
 sort?
 """
 
+"""
+约定:
+    一个模型必须有id,类型为PrimaryKeyField作为它的主键
+    不能自己在模型里面自己定义任何方法,或不属于Fields类型的字段
+"""
+
 def Mysql(host, user, password, database):
     return pymysql.connect(host, user, password, database)
 
-# 将值转安全转意
-def safe(content):
-    if isinstance(content, str):
-        return pymysql.escape_string(content)
+# 将值转安全转意,防止注入
+def safe(value):
+    if isinstance(value, str):
+        return pymysql.escape_string(value)
     else:
-        return content
+        return value
 
 # ===Fields===
 class TextField:
@@ -26,7 +32,8 @@ class TextField:
         self.nullable = nullable
         self.unique = unique
 
-    def _make_sentence(self):
+    # 每个字段都实现这个方法:获取这个字段的单独建表元素比如username VARCHAR(255) NOT NULL UNIQUE
+    def _make_element(self):
         sentence = '{} TEXT'.format(self.fieldname)
         if not self.nullable:
             sentence += ' NOT NULL'
@@ -34,19 +41,22 @@ class TextField:
             sentence += ' UNIQUE'
         return sentence
 
+    # 每个字段都实现这个方法:检查值是否符合要求
     def _check(self, value):
+        # 类型不匹配或者nullable=False且值为空都不能通过验证
         if isinstance(value, str) or (value is None and self.nullable):
             return True
         return False
     
-    def _add(self, value):
+    # 每个字段都实现这个方法:获取这个字段值的表达式比如INT和FLOAT分别是1,1.0,而TEXT,VARCHAR是'1'
+    def _value(self, value):
         return "'{}'".format(value)
     
     def __eq__(self, value):
-        return '{}={}'.format(self.fieldname, self._add(value))
+        return '{}={}'.format(self.fieldname, self._value(value))
     
     def __ne__(self, value):
-        return '{}!={}'.format(self.fieldname, self._add(value))
+        return '{}!={}'.format(self.fieldname, self._value(value))
 
 class VarcharField:
     def __init__(self, max_length=256, nullable=True, unique=False, default=None):
@@ -56,7 +66,7 @@ class VarcharField:
         self.unique = unique
         self.default = default
 
-    def _make_sentence(self):
+    def _make_element(self):
         sentence = '{} VARCHAR({})'.format(self.fieldname, self.max_length)
         if self.default is not None:
             sentence += " DEFAULT '{}'".format(self.default)
@@ -71,14 +81,14 @@ class VarcharField:
             return True
         return False
     
-    def _add(self, value):
+    def _value(self, value):
         return "'{}'".format(value)
     
     def __eq__(self, value):
-        return '{}={}'.format(self.fieldname, self._add(value))
+        return '{}={}'.format(self.fieldname, self._value(value))
     
     def __ne__(self, value):
-        return '{}!={}'.format(self.fieldname, self._add(value))
+        return '{}!={}'.format(self.fieldname, self._value(value))
         
 class IntField:
     def __init__(self, nullable=True, unique=False, default=None):
@@ -87,7 +97,7 @@ class IntField:
         self.unique = unique
         self.default = default
 
-    def _make_sentence(self):
+    def _make_element(self):
         sentence = '{} INT'.format(self.fieldname)
         if self.default is not None:
             sentence += " DEFAULT {}".format(self.default)
@@ -102,26 +112,26 @@ class IntField:
             return True
         return False
     
-    def _add(self, value):
+    def _value(self, value):
         return "{}".format(value)
 
     def __eq__(self, value):
-        return '{}={}'.format(self.fieldname, self._add(value))
+        return '{}={}'.format(self.fieldname, self._value(value))
     
     def __ne__(self, value):
-        return '{}!={}'.format(self.fieldname, self._add(value))
+        return '{}!={}'.format(self.fieldname, self._value(value))
     
     def __gt__(self, value):
-        return '{}>{}'.format(self.fieldname, self._add(value))
+        return '{}>{}'.format(self.fieldname, self._value(value))
     
     def __lt__(self, value):
-        return '{}<{}'.format(self.fieldname, self._add(value))
+        return '{}<{}'.format(self.fieldname, self._value(value))
     
     def __ge__(self, value):
-        return '{}>={}'.format(self.fieldname, self._add(value))
+        return '{}>={}'.format(self.fieldname, self._value(value))
 
     def __le__(self, value):
-        return '{}<={}'.format(self.fieldname, self._add(value))
+        return '{}<={}'.format(self.fieldname, self._value(value))
 
 class FloatField:
     def __init__(self, nullable=True, unique=False, default=None):
@@ -130,7 +140,7 @@ class FloatField:
         self.unique = unique
         self.default = default
 
-    def _make_sentence(self):
+    def _make_element(self):
         sentence = '{} FLOAT'.format(self.fieldname)
         if self.default is not None:
             sentence += " DEFAULT {}".format(self.default)
@@ -145,59 +155,59 @@ class FloatField:
             return True
         return False
     
-    def _add(self, value):
+    def _value(self, value):
         return "{}".format(value)
     
     def __eq__(self, value):
-        return '{}={}'.format(self.fieldname, self._add(value))
+        return '{}={}'.format(self.fieldname, self._value(value))
     
     def __ne__(self, value):
-        return '{}!={}'.format(self.fieldname, self._add(value))
+        return '{}!={}'.format(self.fieldname, self._value(value))
     
     def __gt__(self, value):
-        return '{}>{}'.format(self.fieldname, self._add(value))
+        return '{}>{}'.format(self.fieldname, self._value(value))
     
     def __lt__(self, value):
-        return '{}<{}'.format(self.fieldname, self._add(value))
+        return '{}<{}'.format(self.fieldname, self._value(value))
     
     def __ge__(self, value):
-        return '{}>={}'.format(self.fieldname, self._add(value))
+        return '{}>={}'.format(self.fieldname, self._value(value))
 
     def __le__(self, value):
-        return '{}<={}'.format(self.fieldname, self._add(value))
+        return '{}<={}'.format(self.fieldname, self._value(value))
 
 class PrimaryKeyField():
     def __init__(self):
-        self.fieldname = None
+        self.fieldname = 'id'
     
     def _check(self, value):
-        if isinstance(value, int) or (value is None and self.nullable):
+        if isinstance(value, int) or value is None:
             return True
         return False
     
-    def _add(self, value):
+    def _value(self, value):
         return '{}'.format(value)
     
-    def _make_sentence(self):
+    def _make_element(self):
         return '{} INT NOT NULL AUTO_INCREMENT PRIMARY KEY'.format(self.fieldname)
     
     def __eq__(self, value):
-        return '{}={}'.format(self.fieldname, self._add(value))
+        return '{}={}'.format(self.fieldname, self._value(value))
     
     def __ne__(self, value):
-        return '{}!={}'.format(self.fieldname, self._add(value))
+        return '{}!={}'.format(self.fieldname, self._value(value))
     
     def __gt__(self, value):
-        return '{}>{}'.format(self.fieldname, self._add(value))
+        return '{}>{}'.format(self.fieldname, self._value(value))
     
     def __lt__(self, value):
-        return '{}<{}'.format(self.fieldname, self._add(value))
+        return '{}<{}'.format(self.fieldname, self._value(value))
     
     def __ge__(self, value):
-        return '{}>={}'.format(self.fieldname, self._add(value))
+        return '{}>={}'.format(self.fieldname, self._value(value))
 
     def __le__(self, value):
-        return '{}<={}'.format(self.fieldname, self._add(value))
+        return '{}<={}'.format(self.fieldname, self._value(value))
         
 
 field_types = (TextField, VarcharField, IntField, FloatField, PrimaryKeyField)
@@ -208,45 +218,44 @@ class Base:
         db = None
         table = None
 
+    # 在每个操作里面都检查是否init,仅仅执行一次init
     _init_sign = False
     _fields = {}
 
-    # 接收值,如果属于某字段就__setattr__
+    # 接收本对象的各个字段值,没有赋值的默认为None
     def __init__(self, **kwargs):
         self.__class__._init()
         # 靠id来判断该对象是否已经被插入进数据库了,如果为None则还未被插入
         fields = self.__class__._get_fields()
         fieldnames = fields.keys()
-        # 检查是否有未知的参数
         for key in kwargs.keys():
             if key not in fieldnames:
                 raise Exception('未知的参数{}'.format(key))
         for key,value in fields.items():
-            self.__setattr__(key,kwargs.get(key))
+            self.__setattr__(key,kwargs.get(key, None))
 
-    # 将字段名插入field字段里面
+    # 将字段名写入_field,将字段名写入对应字段对象,并标记已初始化
     @classmethod
     def _init(cla):
         if not cla._init_sign:
             for key,value in cla.__dict__.items():
                 if isinstance(value, field_types):
+                    value.fieldname = key
                     cla._fields[key] = value
-            fields = cla._get_fields()
-            for key,value in fields.items():
-                value.fieldname = key
             cla._init_sign = True
 
-    # 识别所有field类变量
+    # 获取_fields
     @classmethod
     def _get_fields(cla):
         return cla._fields
     
+    # 获取当前对象的数据
     def _get_current_data(self):
         fields = self.__class__._get_fields()
         res = {}
         for fieldname in fields.keys():
             value = self.__getattribute__(fieldname)
-            # 防止被类变量覆盖
+            # 防止没有赋值该属性导致被类变量覆盖
             if not isinstance(value,field_types):
                 res[fieldname] = self.__getattribute__(fieldname)
             else:
@@ -259,16 +268,13 @@ class Base:
         db = cla.Meta.db
         table = cla.Meta.table
         cursor = db.cursor()
-
         fields = cla._get_fields()
 
-        field_sentences = []
+        field_elements = []
         for key,value in fields.items():
-            field_sentences.append(value._make_sentence())
+            field_elements.append(value._make_element())
         
-        field_str = ','.join(field_sentences)
-
-        sentence = 'CREATE TABLE IF NOT EXISTS {} ({})'.format(table, field_str)
+        sentence = 'CREATE TABLE IF NOT EXISTS {} ({})'.format(table, ','.join(field_elements))
         cursor.execute(sentence)
     
     @classmethod
@@ -288,16 +294,15 @@ class Base:
         data = self._get_current_data()
         fields = self.__class__._get_fields()
 
-
         fieldnames = []
         values = []
         # 类型检查
         for key,value in fields.items():
-            if key == 'id':
-                continue
             if value._check(data.get(key)):
+                if key == 'id':
+                    continue
                 fieldnames.append(key)
-                values.append(value._add((safe((data.get(key))))))
+                values.append(value._value((safe((data.get(key))))))
             else:
                 raise TypeError('类型不匹配')
         fieldnames_str = ','.join(fieldnames)
@@ -352,7 +357,7 @@ class Base:
             value = current_data.get(field.fieldname)
             if not field._check(value):
                 raise TypeError('类型错误')
-            sets.append('{}={}'.format(field.fieldname,field._add(safe(value))))
+            sets.append('{}={}'.format(field.fieldname,field._value(safe(value))))
         temp = ','.join(sets)
         sentence = 'UPDATE {} SET {} WHERE id={}'.format(table, temp, str(self.id))
         cursor.execute(sentence)
